aac57afbac6a5c79894474a3941846611d222591
Max <Nirvana0099@mail.ru>
2024-08-20 06:37:24 +0800

    Corrections!

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
         for (int i = 1; i < 11; i = i + 1 ) {
-            System.out.println(" Интерация цикла " + i);
+            System.out.println(i);
         }
         System.out.println(" Задание 2");
         for (int i = 10; i > 0; i = i - 1) {
-            System.out.println(" Интерация цикла " + i);
+            System.out.println(i);
         }
         System.out.println(" Задание 3");
         for (int i = 0; i < 18; i = i + 2) {
-            System.out.println(" четное число " + i);
+            System.out.println(i);
         }
         System.out.println(" Задание 4");
         for (int i = 10; i > -11; i = i - 1) {
-            System.out.println(" Интерация цикла " + i);
+            System.out.println(i);
         }
         System.out.println(" Задание 5");
         for (int i = 1904; i < 2097; i = i + 4) {
-            System.out.println(" Високосный год " + i);
+            System.out.println(" Год является високосным " + i);
         }
         System.out.println(" Задание 6");
         for (int i = 7; i < 99; i = i + 7) {
